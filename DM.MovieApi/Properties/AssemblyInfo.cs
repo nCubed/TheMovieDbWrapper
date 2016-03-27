2befb3d1cc04d77ee627880d51c7e7bcfaaca977
kindler chase <kindler@gmail.com>
2016-03-27 15:29:25 -0500

    Updated assembly information.

--- DM.MovieApi/Properties/AssemblyInfo.cs
+++ DM.MovieApi/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "nCubed" )]
-[assembly: AssemblyProduct( "Digital Media" )]
+[assembly: AssemblyProduct( "TheMovieDb.org Wrapper" )]
 [assembly: AssemblyCopyright( "Copyright © nCubed 2015" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "0.0.1.0" )]
-[assembly: AssemblyFileVersion( "0.0.1.0" )]
+[assembly: AssemblyVersion( "0.1.0.0" )]
+[assembly: AssemblyFileVersion( "0.1.0.0" )]
 
 [assembly: InternalsVisibleTo( "DM.MovieApi.IntegrationTests" )]
