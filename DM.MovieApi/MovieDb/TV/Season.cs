e334b28d6faaf2be9edf5800f73dec08736d3f5e
yeah-buddy <matt.mccabe1290@gmail.com>
2016-05-26 02:31:30 -0500

    Adding Api Request wrapper for searching for TV shows by name, getting the latest show, finding a show by id, getting the top rated shows, and getting popular shows.  Adding in models to represent the TV show, the Network, season information and TV show creator.  Adding in integration tests to prove out the new Api Request.

---
+++ DM.MovieApi/MovieDb/TV/Season.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DM.MovieApi.MovieDb.TV
+{
+    [DataContract]
+    public class Season
+    {
+        [DataMember( Name = "id" )]
+        public int Id { get; set; }
+
+        [DataMember( Name = "air_date" )]
+        public DateTime AirDate { get; set; }
+
+        [DataMember( Name = "episode_count" )]
+        public int EpisodeCount { get; set; }
+
+        [DataMember( Name = "poster_path" )]
+        public string PosterPath { get; set; }
+
+        [DataMember( Name = "season_number" )]
+        public int SeasonNumber { get; set; }
+
+        public Season( int id, DateTime airDate, int episodeCount, string posterPath, int seasonNumber )
+        {
+            Id = id;
+            AirDate = airDate;
+            EpisodeCount = episodeCount;
+            PosterPath = posterPath;
+            SeasonNumber = seasonNumber;
+        }
+
+        public override string ToString()
+        {
+            return string.Format( "({0} - {1:yyyy-MM-dd})", SeasonNumber, AirDate );
+        }
+    }
+}
