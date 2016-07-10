71222824747bbad82a9975e0ae65ad79a3444fc8
kindler chase <kindler@gmail.com>
2016-07-09 19:30:46 -0500

    Added code comments to new people request API.

---
+++ DM.MovieApi/MovieDb/People/ApiPeopleRequest.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using DM.MovieApi.ApiRequest;
+using DM.MovieApi.ApiResponse;
+
+namespace DM.MovieApi.MovieDb.People
+{
+    [Export( typeof( IApiPeopleRequest ) )]
+    [PartCreationPolicy( CreationPolicy.NonShared )]
+    internal class ApiPeopleRequest : ApiRequestBase, IApiPeopleRequest
+    {
+        [ImportingConstructor]
+        public ApiPeopleRequest( IMovieDbSettings settings )
+            : base( settings )
+        { }
+
+        public async Task<ApiQueryResponse<Person>> FindByIdAsync( int personId, string language = "en" )
+        {
+            var param = new Dictionary<string, string>
+            {
+                {"language", language}
+            };
+
+            string command = $"person/{personId}";
+
+            ApiQueryResponse<Person> response = await base.QueryAsync<Person>( command, param );
+
+            return response;
+        }
+
+        public async Task<ApiQueryResponse<PersonMovieCredit>> GetMovieCreditsAsync( int personId, string language = "en" )
+        {
+            var param = new Dictionary<string, string>
+            {
+                {"language", language},
+            };
+
+            string command = $"person/{personId}/movie_credits";
+
+            ApiQueryResponse<PersonMovieCredit> response = await base.QueryAsync<PersonMovieCredit>( command, param );
+
+            return response;
+        }
+    }
+}
--- DM.MovieApi/MovieDb/People/IApiPeopleRequest.cs
+++ DM.MovieApi/MovieDb/People/IApiPeopleRequest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DM.MovieApi.ApiRequest;
 using DM.MovieApi.ApiResponse;
 
@@ -8,49 +6,23 @@ namespace DM.MovieApi.MovieDb.People
 {
     public interface IApiPeopleRequest : IApiRequest
     {
+        /// <summary>
+        /// Gets all the information about a specific Person.
+        /// </summary>
+        /// <param name="personId">The person Id is typically found from a more generic query such as movie or television or search.</param>
+        /// <param name="language">Default is English. The ISO 639-1 language code to retrieve the result from.</param>
         Task<ApiQueryResponse<Person>> FindByIdAsync( int personId, string language = "en" );
 
+
+        /// <summary>
+        /// Get the movie credits for a specific person id. Includes movie cast and crew information for the person.
+        /// </summary>
+        /// <param name="personId">The person Id is typically found from a more generic query such as movie or television or search.</param>
+        /// <param name="language">Default is English. The ISO 639-1 language code to retrieve the result from.</param>
         Task<ApiQueryResponse<PersonMovieCredit>> GetMovieCreditsAsync( int personId, string language = "en" );
+
 
         // TODO: (K. Chase) [2016-07-09] Add GetTelevisionCreditsAsync
         // TODO: (K. Chase) [2016-07-09] Add SearchAsync
-    }
-
-    [Export( typeof( IApiPeopleRequest ) )]
-    [PartCreationPolicy( CreationPolicy.NonShared )]
-    internal class ApiPeopleRequest : ApiRequestBase, IApiPeopleRequest
-    {
-        [ImportingConstructor]
-        public ApiPeopleRequest( IMovieDbSettings settings )
-            : base( settings )
-        { }
-
-        public async Task<ApiQueryResponse<Person>> FindByIdAsync( int personId, string language = "en" )
-        {
-            var param = new Dictionary<string, string>
-            {
-                {"language", language}
-            };
-
-            string command = $"person/{personId}";
-
-            ApiQueryResponse<Person> response = await base.QueryAsync<Person>( command, param );
-
-            return response;
-        }
-
-        public async Task<ApiQueryResponse<PersonMovieCredit>> GetMovieCreditsAsync( int personId, string language = "en" )
-        {
-            var param = new Dictionary<string, string>
-            {
-                {"language", language},
-            };
-
-            string command = $"person/{personId}/movie_credits";
-
-            ApiQueryResponse<PersonMovieCredit> response = await base.QueryAsync<PersonMovieCredit>( command, param );
-
-            return response;
-        }
     }
 }
