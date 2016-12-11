de1093f4531a788664debd76c65a9cd31718df37
kindler chase <kindler@gmail.com>
2016-12-11 11:38:34 -0600

    The Force Awakens is losing the popular vote.

--- DM.MovieApi.IntegrationTests/MovieDb/Movies/ApiMovieRequestTests.cs
+++ DM.MovieApi.IntegrationTests/MovieDb/Movies/ApiMovieRequestTests.cs
@@ -130,7 +130,6 @@ namespace DM.MovieApi.IntegrationTests.MovieDb.Movies
             ApiResponseUtil.AssertImagePath( movie.PosterPath );
 
             Assert.IsTrue( movie.Overview.StartsWith( expetedOverview ) );
-            Assert.IsTrue( movie.Popularity > 9, $"Actual: {movie.Popularity}" );
             Assert.IsTrue( movie.VoteAverage > 5 );
             Assert.IsTrue( movie.VoteCount > 1500 );
 
