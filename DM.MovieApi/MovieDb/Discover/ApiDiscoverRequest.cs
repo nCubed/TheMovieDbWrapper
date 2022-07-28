fa90a734e5576869adf01363fff56f112f031df3
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-28 11:14:42 +0300

    create discover request api

---
+++ DM.MovieApi/MovieDb/Discover/ApiDiscoverRequest.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DM.MovieApi.ApiRequest;
+using DM.MovieApi.ApiResponse;
+using DM.MovieApi.MovieDb.Genres;
+using DM.MovieApi.MovieDb.Movies;
+using DM.MovieApi.Shims;
+
+namespace DM.MovieApi.MovieDb.Discover
+{
+    internal class ApiDisoverRequest : ApiRequestBase, IApiDiscoverRequest
+    {
+        private readonly IApiGenreRequest _genreApi;
+
+        [ImportingConstructor]
+        public ApiDisoverRequest(IApiSettings settings, IApiGenreRequest genreApi) : base(settings)
+        {
+            _genreApi = genreApi;
+        }
+
+        public async Task<ApiSearchResponse<MovieInfo>> DiscoverMoviesAsync(IDiscoverParameterBuilder paramBuilder, int pageNumber = 1, string language = "en")
+        {
+            var param = paramBuilder.Build();
+            param.Add("language", language);
+            const string command = "discover/movie";
+
+            ApiSearchResponse<MovieInfo> response = await base.SearchAsync<MovieInfo>(command, pageNumber, param);
+            response.Results.PopulateGenres(_genreApi);
+            return response;
+        }
+    }
+}
---
+++ DM.MovieApi/MovieDb/Discover/IApiDiscoverRequest.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DM.MovieApi.ApiRequest;
+using DM.MovieApi.ApiResponse;
+using DM.MovieApi.MovieDb.Movies;
+
+namespace DM.MovieApi.MovieDb.Discover
+{
+    public interface IApiDiscoverRequest : IApiRequest
+    {
+        Task<ApiSearchResponse<MovieInfo>> DiscoverMoviesAsync(IDiscoverParameterBuilder paramBuilder, int pageNumber  = 1,string language = "en");
+    }
+}
