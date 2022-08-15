b94e4667209a8c243ee9b82f74bc8949265125c2
kindler chase <kindler@gmail.com>
2022-08-15 11:10:45 -0500

    Add error checking in DiscoverMoviesAsync.
    Add error checking in DiscoverMoviesAsync; update conventions to follow repo conventions.

--- DM.MovieApi/MovieDb/Discover/ApiDiscoverRequest.cs
+++ DM.MovieApi/MovieDb/Discover/ApiDiscoverRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DM.MovieApi.ApiRequest;
 using DM.MovieApi.ApiResponse;
 using DM.MovieApi.MovieDb.Genres;
@@ -12,19 +13,28 @@ namespace DM.MovieApi.MovieDb.Discover
         private readonly IApiGenreRequest _genreApi;
 
         [ImportingConstructor]
-        public ApiDiscoverRequest( IApiSettings settings, IApiGenreRequest genreApi ) : base( settings )
+        public ApiDiscoverRequest( IApiSettings settings, IApiGenreRequest genreApi )
+            : base( settings )
         {
             _genreApi = genreApi;
         }
 
         public async Task<ApiSearchResponse<MovieInfo>> DiscoverMoviesAsync( IDiscoverMovieParameterBuilder paramBuilder, int pageNumber = 1, string language = "en" )
         {
-            var param = paramBuilder.Build();
+            Dictionary<string, string> param = paramBuilder.Build();
             param.Add( "language", language );
+
             const string command = "discover/movie";
 
             ApiSearchResponse<MovieInfo> response = await base.SearchAsync<MovieInfo>( command, pageNumber, param );
+
+            if( response.Error != null )
+            {
+                return response;
+            }
+
             response.Results.PopulateGenres( _genreApi );
+
             return response;
         }
     }
