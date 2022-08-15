ca95bc847cb36fbb50ed2d1c45a7baa217df4e39
kindler chase <kindler@gmail.com>
2022-08-15 11:13:38 -0500

    Delete unneeded parameter builder; should be on the main interface.

--- DM.MovieApi/MovieDb/Discover/IDiscoverMovieParameterBuilder.cs
+++ DM.MovieApi/MovieDb/Discover/IDiscoverMovieParameterBuilder.cs
@@ -1,7 +1,10 @@
-﻿namespace DM.MovieApi.MovieDb.Discover
+﻿using System.Collections.Generic;
+
+namespace DM.MovieApi.MovieDb.Discover
 {
-    public interface IDiscoverMovieParameterBuilder : IDiscoverParameterBuilder
+    public interface IDiscoverMovieParameterBuilder
     {
+        Dictionary<string, string> Build();
         IDiscoverMovieParameterBuilder WithOriginalLanguage( string language );
         IDiscoverMovieParameterBuilder WithCrew( int personId );
         IDiscoverMovieParameterBuilder WithCast( int personId );
--- DM.MovieApi/MovieDb/Discover/IDiscoverParameterBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DM.MovieApi.MovieDb.Discover
-{
-    public interface IDiscoverParameterBuilder
-    {
-        Dictionary<string, string> Build();
-    }
-}
